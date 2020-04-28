032d762d36481e929208188c0eacf8047b3c5671
dqmrf <hexnomad@meta.ua>
2020-04-28 19:05:05 +0300

    rework container methods - add: _resolveTargetDependencies - change: _resolveTarget - change: _getElementByName

--- container.js
+++ container.js
@@ -1,8 +1,9 @@
 const { Lifetime } = require('./constants');
 const { isComponent, isGroup } = require('./utils');
 
+module.exports = { createContainer };
 
-const createContainer = () => {
+function createContainer() {
   const resolutionStack = [];
   const cache = new Map();
   const _elements = {};
@@ -77,66 +78,56 @@ const createContainer = () => {
     const dependencies = options.dependsOn;
 
     if (dependencies && dependencies.length > 0) {
-      let children = {};
-
-      dependencies.forEach(dependency => {
-        const resolved = resolve(dependency);
-        Object.assign(children, _createDependencyMap(dependency, resolved));
-      });
-
-      return target(children);
+      return target(_resolveTargetDependencies(dependencies));
     }
 
     return target();
   }
 
+  function _resolveTargetDependencies(dependencies) {
+    let resolvedDependencies = {};
+    dependencies.forEach(dependency => {
+      const resolvedDependency = resolve(dependency);
+      const newDependency = _createDependencyMap(dependency, resolvedDependency);
+      Object.assign(resolvedDependencies, newDependency);
+    });
+    return resolvedDependencies;
+  }
+
   function _createDependencyMap(name, dependency) {
     const subnames = name.split('.');
-    return _generate(subnames);
 
-    function _generate(paths) {
+    return _create(subnames);
+
+    function _create(paths) {
       const currentPath = paths.shift();
       if (!currentPath) return dependency;
-      return { [currentPath]: _generate(paths) }
+      return { [currentPath]: _create(paths) }
     }
   }
 
   function _getElementByName(name) {
-    let currentElement, result;
     const subnames = name.split('.');
 
-    subnames.forEach((subname, index) => {
-      // First iteration:
-      if (index === 0) currentElement = _elements;
+    return _find(subnames, _elements);
 
-      // Middle itarations:
-      if (index + 1 !== subnames.length) {
-        // currentElement = GROUP element.
-        currentElement = currentElement[subname];
+    function _find(paths, elements) {
+      const isLastPath = !paths[1],
+            currentPath = paths.shift(),
+            currentElement = elements[currentPath];
 
+      if (!isLastPath) {
         if (isGroup(currentElement)) {
-          currentElement = currentElement.components;
-        } else {
-          // Components are inappropriate in middle iterations.
-          throw new Error(`Incorrect registration name: '${name}'.`);
+          return _find(paths, currentElement.components);
+        }
+        // TODO: else { here will be groups resolving implementation. }
+      } else {
+        if (isComponent(currentElement)) {
+          return currentElement;
         }
       }
 
-      // Last iteration:
-      else {
-        result = currentElement[subname];
-      }
-    });
-
-    if (!result) {
-      // TODO: DRY
       throw new Error(`Incorrect registration name: '${name}'.`);
     }
-
-    return result;
   }
 }
-
-module.exports = {
-  createContainer
-};
