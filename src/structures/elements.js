cc560a5c5543755970ef3125568471461b167112
dqmrf <hexnomad@meta.ua>
2020-05-05 00:02:13 +0300

    Element structures optimization.

--- src/structures/elements.js
+++ src/structures/elements.js
@@ -1,16 +1,36 @@
-const t = require('tcomb-validation');
+const t = require('../infra/tcomb');
 const { LifetimeEnums, ComponentTypesEnums } = require('./enums');
 const { ElementTypes, ComponentTypes, Lifetime, SFIOC } = require('../constants');
 
+const ElementBase = t.declare('ElementBase');
+const ComponentBase = t.declare('ComponentBase');
 const Component = t.declare('Component');
 const ComponentOptions = t.declare('ComponentOptions');
+const GroupBase = t.declare('GroupBase');
 const Group = t.declare('Group');
-const ComponentOrGroup = t.declare('Component | Group');
+const GroupRecursive = t.declare('GroupRecursive');
+const Element = t.declare('Component | Group');
 const Elements = t.declare('Elements');
 
-Component.define(t.struct({
-  _sfType: t.refinement(t.String, t => t === SFIOC.ELEMENT),
-  _sfElementType: t.refinement(t.String, t => t === ElementTypes.COMPONENT),
+// Element.
+ElementBase.define(t.struct({
+  _sfType: t.refinement(t.String, t => t === SFIOC.ELEMENT)
+}));
+
+Element.define(t.refinement(t.Object, (obj) => {
+  const componentResult = t.validate(obj, Component).isValid();
+  const groupResult = t.validate(obj, Group).isValid();
+  return componentResult || groupResult;
+}));
+
+Elements.define(t.dict(t.String, Element));
+
+// Component.
+ComponentBase.define(ElementBase.extend({
+  _sfElementType: t.refinement(t.String, t => t === ElementTypes.COMPONENT)
+}));
+
+Component.define(ComponentBase.extend({
   target: t.Function,
   options: ComponentOptions
 }));
@@ -26,24 +46,27 @@ ComponentOptions.define(t.struct({
   }
 }));
 
-Group.define(t.struct({
-  _sfType: t.refinement(t.String, t => t === SFIOC.ELEMENT),
-  _sfElementType: t.refinement(t.String, t => t === ElementTypes.GROUP),
+// Group.
+GroupBase.define(ElementBase.extend({
+  _sfElementType: t.refinement(t.String, t => t === ElementTypes.GROUP)
+}));
+
+Group.define(GroupBase.extend({
+  elements: t.Object
+}));
+
+GroupRecursive.define(GroupBase.extend({
   elements: Elements
 }));
 
-ComponentOrGroup.define(t.refinement(t.Object, (obj) => {
-  const componentResult = t.validate(obj, Component).isValid();
-  const groupResult = t.validate(obj, Group).isValid();
-  return componentResult || groupResult;
-}));
-
-Elements.define(t.dict(t.String, ComponentOrGroup));
-
 module.exports = {
+  ElementBase,
+  Element,
+  Elements,
+  ComponentBase,
   Component,
   ComponentOptions,
+  GroupBase,
   Group,
-  ComponentOrGroup,
-  Elements
+  GroupRecursive
 };
